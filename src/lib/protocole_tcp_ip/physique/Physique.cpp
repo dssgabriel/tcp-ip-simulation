d7b688f4a0a41ad5436e065d5a9180449b6c908a
QGruchet <55387771+QGruchet@users.noreply.github.com>
2021-05-11 00:27:36 +0200

    Ajout documentation (manque la doc pour les checksum) (#17)
    * Modify tripleACK for boolean
    
    * begin doc for Internet.cpp
    
    * Add documentation 2

--- src/lib/protocole_tcp_ip/physique/Physique.cpp
+++ src/lib/protocole_tcp_ip/physique/Physique.cpp
@@ -6,70 +6,73 @@
 #include "Physique.hpp"
 
 /**
-  * Description :
-  * Fonction :
-  * Parametres : 
-  * Sortie :
+  * @brief Constructeur de la classe Physique.
+  * 
+  * Le constructeur est vide car nous utilisons les setters pour initialiser les differents paramètres.
+  * 
+  * @return NULL.
   **/
 Physique::Physique() {
     
 }
 
- /**
-  * Description :
-  * Fonction :
-  * Parametres : 
-  * Sortie :
+/**
+  * @brief Destructeur de la classe Physique.
+  * 
+  * Le destructeur est vide car tout est géré par le 'garbage collector'.
+  * 
+  * @return NULL.
   **/
 Physique::~Physique() {
 
 }
 
- /**
-  * Description :
-  * Fonction :
-  * Parametres : 
-  * Sortie :
+/**
+  * @brief Setter de l'attribut de classe m_SrcMac.
+  * 
+  * @param src : Adresse MAC source souhaitée.
+  * @return void.
   **/
 void Physique::setMacSrc(MAC src) {
     m_SrcMac = src;
 }
 
- /**
-  * Description :
-  * Fonction :
-  * Parametres : 
-  * Sortie :
+/**
+  * @brief Getter de l'attribut de classe m_SrcMac.
+  *
+  * @return La valeur de l'adresse MAC source.
   **/
 MAC Physique::getMacSrc() {
     return m_SrcMac;
 }
 
- /**
-  * Description :
-  * Fonction :
-  * Parametres : 
-  * Sortie :
+/**
+  * @brief Setter de l'attribut de classe m_DestMac.
+  * 
+  * @param dest : Adresse destination souhaitée.
+  * @return void.
   **/
 void Physique::setMacDest(MAC dest) {
     m_DestMac = dest;
 }
 
- /**
-  * Description :
-  * Fonction :
-  * Parametres : 
-  * Sortie :
+/**
+  * @brief Getter de l'attribut de classe m_DestMac.
+  * 
+  * @return La valeur de l'adresse MAC de destination.
   **/
 MAC Physique::getMacDest() {
     return m_DestMac;
 }
 
- /**
-  * Description :
-  * Fonction :
-  * Parametres : 
-  * Sortie :
+/**
+  * @brief Permet de convertir une adresse MAC en un std::bitset de 48 bits en vue de l'encapsuler.
+  * 
+  * On converti d'abord chaque champs de la structure MAC en chaine de caractère. 
+  * Puis nous créeons un bitset de la concaténation de ces chaines de caractère. 
+  * 
+  * @param adresse : L'adresse MAC que l'on souhaite convertir.
+  * @return Le bitset resultant de la convertion.
   **/
 std::bitset<48> Physique::convertir(const MAC& adresse) {
 	std::string s1 = adresse.a.to_string();
@@ -81,11 +84,16 @@ std::bitset<48> Physique::convertir(const MAC& adresse) {
 	return std::bitset<48>(s1 + s2 + s3 + s4 + s5 + s6);
 }
 
- /**
-  * Description :
-  * Fonction :
-  * Parametres : 
-  * Sortie :
+/**
+  * @brief Permet de convertir deux bitset de 16 bits en une structure MAC.
+  * 
+  * Après désencapsulation, les adresses MAC sont contenues dans trois std::bitset<16> ou chaque 8 bits représente un champ de l'adresse MAC.
+  * Cette fonction permet donc de diviser ces bitset en deux, pour remplir la MAC.
+  * 
+  * @param adrPartBA : Premier bitset contenant les champs a et b.
+  * @param adrPartDC : Second bitset contenant les champs c et d.
+  * @param adrPartFE : Troisième bitset contenant les champs e et f.
+  * @return Une structure MAC avec les champs remplie.
   **/
 MAC Physique::convertir(const std::bitset<16>& adrPartBA, 
     const std::bitset<16>& adrPartDC, const std::bitset<16>& adrPartFE) 
@@ -93,7 +101,8 @@ MAC Physique::convertir(const std::bitset<16>& adrPartBA,
   // Initialisation de l'adress MAC.
   MAC mac;
 
-  //
+  //division en deux de chaque bitset de 16 bits
+  //et association dans les champs de mac;
   std::bitset<8> gauche, droite;
 
   diviser(adrPartBA, droite, gauche);
@@ -110,11 +119,12 @@ MAC Physique::convertir(const std::bitset<16>& adrPartBA,
 
   return mac;
 }
- /**
-  * Description :
-  * Fonction :
-  * Parametres : 
-  * Sortie :
+
+/**
+  * @brief Permet de convertir un bitset de 48 bits en une pile de bitset de 16 bits.
+  * 
+  * @param adresse Le bitset que l'on veut diviser.
+  * @return La pile contenant l'argument découpé en plusieurs morceaux.
   **/
 std::stack<std::bitset<16>> Physique::decoupageMac(const std::bitset<48>& adresse) {
   std::stack<std::bitset<16>> pile;
@@ -142,11 +152,11 @@ std::stack<std::bitset<16>> Physique::decoupageMac(const std::bitset<48>& adress
   return pile;
 }
 
- /**
-  * Description :
-  * Fonction :
-  * Parametres : 
-  * Sortie :
+/**
+  * @brief Permet l'encapsulation de la couche Physique.
+  * 
+  * @param paquet : Resultat de l'encapsulation de la couche Internet.
+  * @return. Le resultat de la désencapsulation. Donc une pile contenant la couche Physique + Internet + Transport.
   **/
  std::stack<std::bitset<16>> Physique::encapsuler(std::stack<std::bitset<16>>& paquet) {
     paquet.push(concat(m_SrcMac.a, m_SrcMac.b));
@@ -159,11 +169,11 @@ std::stack<std::bitset<16>> Physique::decoupageMac(const std::bitset<48>& adress
     return paquet;
  }
 
- /**
-  * Description :
-  * Fonction :
-  * Parametres : 
-  * Sortie :
+/**
+  * @brief Permet la desencapsulation de la couche Physique.
+  * 
+  * @param trame. Une pile qui contitent la couche Transport + Internet + Physique. 
+  * @return La même pile mais avec la couche Physique en moins.
   **/
 std::stack<std::bitset<16>> Physique::desencapsuler(std::stack<std::bitset<16>>& trame) {
     for (size_t i = 0; i < 6; i++)
