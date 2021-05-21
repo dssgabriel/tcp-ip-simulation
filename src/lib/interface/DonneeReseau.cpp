e05bf9d545951e37d48a59439ce7046315d70379
uvsq21805057 <72023438+uvsq21805057@users.noreply.github.com>
2021-05-21 14:23:35 +0200

    Méthode rafraichirGraphe() + Doc DonneeReseau (#46)
    * Ajout doc AffichageReseau.cpp + AffichageReseau.h
    
    * Méthode rafraichir graphe() + Doc DonneeReseau
    
    * Méthode rafraichir graphe() + Doc DonneeReseauV2
    
    Co-authored-by: Utilisateur <user@debian.arcahe.ovh>

--- src/lib/interface/DonneeReseau.cpp
+++ src/lib/interface/DonneeReseau.cpp
@@ -1,5 +1,20 @@
+/**
+ * @file        DonneeReseau.cpp
+ * @brief       Vous trouverez ici toutes les fonctions implementées pour la classe DonneeReseau.
+ * 
+ * @author      Johann RAMANANDRAITSIORY
+ * @date        2021
+ **/
+
 #include "DonneeReseau.hpp"
 
+ /**
+  * @brief Constructeur de la classe DonneeReseau.
+  * 
+  * Le constructeur permet de créer deux fenêtres pour:
+  * Exposer le graphique du contrôle de congestion + la configuration du réseau.
+  * Afficher via la classe "ConfigReseau" le temps émis pour l'envoi de paquets.
+  **/
 DonneeReseau::DonneeReseau() : QVBoxLayout()
 {
     // Creation du premier QHboxLayout
@@ -11,7 +26,12 @@ DonneeReseau::DonneeReseau() : QVBoxLayout()
     addLayout(m_F2);
 }
 
-// Destructeur //
+ /**
+  * @brief Destructeur de la classe DonneeReseau.
+  * 
+  * Le destructeur est vide car les classes de Qt s'autodétruisent correctement.
+  * 
+  **/
 DonneeReseau::~DonneeReseau() {
 
 }
